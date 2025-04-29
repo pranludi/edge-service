ae4f94af2637d4c2f16319b2db2a92ec5180f710
daekwon <pranludi@gmail.com>
2025-04-29 09:26:38 +0900

    fix

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -16,6 +17,7 @@ import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
